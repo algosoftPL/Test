3b70240fb4f5e1a4651b1a11c6fb99c02edb8896
Michał "phoe" Herda <phoe@openmailbox.org>
2016-05-30 16:05:52 +0200

    Added test for createIdentityMatrix.

--- src/test/java/matrixlibrary/TestIMatrixImpl.java
+++ src/test/java/matrixlibrary/TestIMatrixImpl.java
@@ -1,6 +1,7 @@
 package matrixlibrary;
 
-import org.junit.Before;
+import static junit.framework.TestCase.assertEquals;
+
 import org.junit.Test;
 
 public class TestIMatrixImpl {
@@ -8,25 +9,30 @@ public class TestIMatrixImpl {
 
     @Test
     public void testCreateIdentityMatrix() {
-        iMatrixImpl = new IMatrixImpl(6, 6);
+        iMatrixImpl = new IMatrixImpl();
         iMatrixImpl.createIdentityMatrix(6);
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                if (i == j) {
+                    assertEquals(iMatrixImpl.getMatrixValue(i, j), 1.0);
+                } else {
+                    assertEquals(iMatrixImpl.getMatrixValue(i, j), 0.0);
+                }
+            }
+        }
     }
     @Test
     public void testDeterminant() {
         iMatrixImpl = new IMatrixImpl(6, 6);}
-
     @Test
     public void testGetMatrixValue() {
         iMatrixImpl = new IMatrixImpl(6, 6);}
-
     @Test
     public void testSetMatrixValue() {
         iMatrixImpl = new IMatrixImpl(6, 6);}
-
     @Test
     public void testSetMatrixValues() {
         iMatrixImpl = new IMatrixImpl(6, 6);}
-
     @Test
     public void testToString() {
         iMatrixImpl = new IMatrixImpl(6, 6);}

802fa523003bf783b83a2c7a44d86011d8649960
k0denik <jakpamula@gmail.com>
2016-05-30 16:53:06 +0200

    IMatrixMath Addition and Subtraction initial implementation

--- src/main/java/matrixlibrary/IMatrixMathImpl.java
+++ src/main/java/matrixlibrary/IMatrixMathImpl.java
@@ -5,16 +5,36 @@ public class IMatrixMathImpl implements IMatrixMath {
         return null;
     }
 
-    public IMatrix matrixAddition(IMatrix m1, IMatrix m2) {
-        return null;
+    public IMatrix matrixAddition(IMatrix m1, IMatrix m2) throws InvalidDimensionException {
+        if(m1.getHeight()!=m2.getHeight() || m1.getWidth()!=m2.getWidth())
+            throw new InvalidDimensionException();
+        double[][] m3 = new double[m1.getWidth()][m1.getHeight()];
+        for (int i = 0; i < m1.getWidth(); i++) {
+            for (int j = 0; j < m1.getHeight(); j++) {
+                m3[i][j] = m1.getMatrixValue(i,j) + m2.getMatrixValue(i,j);
+            }
+        }
+        IMatrix result = new IMatrixImpl();
+        result.setMatrixValues(m3);
+        return result;
     }
 
     public IMatrix matrixMultiplication(IMatrix m1, IMatrix m2) {
         return null;
     }
 
-    public IMatrix matrixSubtracting(IMatrix m1, IMatrix m2) {
-        return null;
+    public IMatrix matrixSubtracting(IMatrix m1, IMatrix m2) throws InvalidDimensionException {
+        if(m1.getHeight()!=m2.getHeight() || m1.getWidth()!=m2.getWidth())
+            throw new InvalidDimensionException();
+        double[][] m3 = new double[m1.getWidth()][m1.getHeight()];
+        for (int i = 0; i < m1.getWidth(); i++) {
+            for (int j = 0; j < m1.getHeight(); j++) {
+                m3[i][j] = m1.getMatrixValue(i,j) - m2.getMatrixValue(i,j);
+            }
+        }
+        IMatrix result = new IMatrixImpl();
+        result.setMatrixValues(m3);
+        return result;
     }
 
     public IMatrix matrixTransposition(IMatrix m1) {
